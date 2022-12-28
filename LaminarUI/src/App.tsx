5a50f27c6f68b14649fab3b5dfe0235c489b869c
Gabriel Meacham <gabe@uxbygm.com>
2022-12-28 14:09:43 -0500

    Install MUI

--- LaminarUI/src/App.tsx
+++ LaminarUI/src/App.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Button from '@mui/material/Button';
+import Switch from '@mui/material/Switch';
 
-import "./index.css";
 
 const App = () => (
   <div className="container">
     <div>Name: LaminarUI</div>
     <div>Framework: react</div>
     <div>Language: TypeScript</div>
-    <div>CSS: Empty CSS</div>
+    <div>CSS: MUI</div>
+    <Button variant="text">MUI Button Test</Button>
+    
+
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
